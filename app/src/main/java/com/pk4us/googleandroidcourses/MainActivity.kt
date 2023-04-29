e8b2764d0c6fef521e3b6aaa76d55091dfb08b1f
PK4US <stepanchuk.ei@gmail.com>
2023-04-29 17:59:54 +0300

    Список с карточками выезжающим скрытым текстом и анимациями

--- app/src/main/java/com/pk4us/googleandroidcourses/MainActivity.kt
+++ app/src/main/java/com/pk4us/googleandroidcourses/MainActivity.kt
@@ -3,13 +3,18 @@ package com.pk4us.googleandroidcourses
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import com.pk4us.googleandroidcourses.jetpackcomposeforandroiddevelopers.composeessentials.MyApp
+import com.pk4us.googleandroidcourses.ui.theme.MyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(text = "Hello World!")
+            MyTheme {
+                MyApp(modifier = Modifier.fillMaxSize())
+            }
         }
     }
 }
