4d01252dcf136173394c26230fd78bb4f5ea8681
PK4US <stepanchuk.ei@gmail.com>
2023-04-30 15:48:49 +0300

    SearchBar, LazyRow, BottomNavigation

--- app/src/main/java/com/pk4us/googleandroidcourses/MainActivity.kt
+++ app/src/main/java/com/pk4us/googleandroidcourses/MainActivity.kt
@@ -4,16 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
-import com.pk4us.googleandroidcourses.jetpackcomposeforandroiddevelopers.composeessentials.MyApp
-import com.pk4us.googleandroidcourses.ui.theme.MyTheme
+import com.pk4us.googleandroidcourses.jetpackcomposeforandroiddevelopers.composeessentials.MySootheApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyTheme {
-                MyApp(modifier = Modifier.fillMaxSize())
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colors.background
+            ) {
+//                MyApp(modifier = Modifier.fillMaxSize())
+
+                MySootheApp()
+
+//                WellnessScreen()
             }
         }
     }
